e924c22fa75c4a66146cbe8565a9b4163dcdd8ac
filippo-baglini <filippo@LAPTOP-EKEAGK63.>
2024-12-19 21:30:07 +0100

    Added grid search with hold out cross validation, still to be tested further

--- tests/test_hold_out.py
+++ tests/test_hold_out.py
@@ -3,6 +3,7 @@ import os
 sys.path.append(os.path.join(sys.path[0], '..'))
 
 from src.utils.data_utils import *
+from src.utils.grid_search import *
 from src.utils.plot import *
 from src.utils.train_val_split import train_val_splitter
 from src.nn import FF_Neural_Network
@@ -16,8 +17,8 @@ import numpy as np
 script_dir = os.path.dirname(__file__)
 
 # Construct the relative path to the data file
-monk_1_train = os.path.join(script_dir, "../data/Monk/monks-1.train")
-monk_1_test = os.path.join(script_dir, "../data/Monk/monks-1.test")
+monk_1_train = os.path.join(script_dir, "../data/Monk/monks-3.train")
+monk_1_test = os.path.join(script_dir, "../data/Monk/monks-3.test")
 
 # Read the data using the constructed path
 x, y =  read_monk_data(monk_1_train)
@@ -25,13 +26,9 @@ x_test, y_true = read_monk_data(monk_1_test)
 x = feature_one_hot_encoding(x, [3,3,2,3,4,2])
 x_test = feature_one_hot_encoding(x_test, [3,3,2,3,4,2])
 
-x_train, x_val, y_train, y_val = train_val_splitter(x, y, 0.3)
+x_train, x_eval, y_train, y_eval = train_val_splitter(x, y, 0.3)
 
-nn = FF_Neural_Network(17, [Dense_layer(17, 4, Leaky_ReLU), Dense_layer(4, 1, Tanh)], Learning_rate(0.01), "Lasso", 0.00001, None, Early_stopping(100, 0.0001))
-
-print(type(nn.layers[-1].activation).__name__)
-
-nn.train(x_train, y_train, 700, x_val, y_val, 'Minibatch', 5)
+nn = grid_search_hold_out(x_train, y_train, x_eval, y_eval)
 
 y_test = np.array([])
 for i in range(len(x_test)):
